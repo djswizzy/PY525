646054824c0839010b2e2977e244fc14eee804f2
djswizzy <31942568+djswizzy@users.noreply.github.com>
2017-10-08 17:59:20 -0400

    Add files via upload

--- HW3Q1.py
+++ HW3Q1.py
@@ -6,7 +6,7 @@ import matplotlib.animation as animation
 size=10
 particles=2
 T=1
-delta=.01
+delta=.001
 time=5
 
 def InitializeParticles(number,boxsize,temperature):
@@ -20,7 +20,6 @@ def InitializeParticles(number,boxsize,temperature):
         ycoords.append(random.uniform(0,boxsize))
         xvel.append(random.gauss(0,sigma))
         yvel.append(random.gauss(0,sigma))
-    print(xcoords)
     return xcoords,ycoords,xvel,yvel;
 
 def ForceFinder(xpts,ypts):
@@ -36,28 +35,35 @@ def ForceFinder(xpts,ypts):
             else:
                 temp.append([0,0])
         force.append(temp)
-    forcex=0
-    forcey=0
     net=[]
     for i in range(len(force)):
+        forcex=0
+        forcey=0
         for j in range(len(force[i])):
             forcex+=force[i][j][0]
             forcey+=force[i][j][1]
         net.append([forcex,forcey])
     return net;
 
-def Evolve(timestep,tmax,xcoord,ycoord,boxsize):
+def Evolve(timestep,tmax,xcoord,ycoord,xinit,yinit,boxsize):
     steps=np.arange(0,tmax,timestep)
     info=[]
+    info.append([xcoord,ycoord])
+    foo=[]
+    bar=[]
+    for j in range(len(xcoord)):
+        foo.append(xcoord[j]+xinit[j]*timestep)
+        bar.append(ycoord[j]+yinit[j]*timestep)
+    info.append([foo,bar])
     for i in range(len(steps)):
         xnew=[]
         ynew=[]
         force=ForceFinder(xcoord,ycoord)
         for j in range(len(xcoord)):
-            xnew.append((2*xcoord[j]-xcoord[j-1]+force[j][0]*(steps[i]-steps[i-1])**2)%boxsize)
-            ynew.append((2*ycoord[j]-ycoord[j-1]+force[j][1]*(steps[i]-steps[i-1])**2)%boxsize)
-            xcoord[j]=(2*xcoord[j]-xcoord[j-1]+force[j][0]*(steps[i]-steps[i-1])**2)%boxsize
-            ycoord[j]=(2*ycoord[j]-ycoord[j-1]+force[j][1]*(steps[i]-steps[i-1])**2)%boxsize
+            xnew.append((2*xcoord[j]-info[i-1][0][j]+force[j][0]*timestep**2)%boxsize)
+            ynew.append((2*ycoord[j]-info[i-1][1][j]+force[j][1]*timestep**2)%boxsize)
+            xcoord[j]=(2*xcoord[j]-info[i-1][0][j]+force[j][0]*timestep**2)%boxsize
+            ycoord[j]=(2*ycoord[j]-info[i-1][1][j]+force[j][1]*timestep**2)%boxsize
         info.append([xnew,ynew])
     return info;
 
@@ -68,16 +74,19 @@ def animate(i, points):
 
 [x,y,xvel,yvel]=InitializeParticles(particles,size,T)
 
-solution=Evolve(delta,time,x,y,size)
-xtime=[solution[i][0] for i in range(len(solution))]
-ytime=[solution[i][1] for i in range(len(solution))]
+solution=Evolve(delta,time,x,y,xvel,yvel,size)
 
 
 #Plotting
+
+
+xtime=[solution[i][0] for i in range(len(solution))]
+ytime=[solution[i][1] for i in range(len(solution))]
+
 
 fig=plt.figure()
 points=plt.plot([],[],'o')
 plt.xlim(0,size)
 plt.ylim(0,size)
-a=animation.FuncAnimation(fig,animate,frames=len(xtime),fargs=(points), interval=25,blit=True)
+a=animation.FuncAnimation(fig,animate,frames=len(xtime),fargs=(points), interval=50,blit=True)
 plt.show()
